ea3314342bbd2c6e4820f527a1be3786552adda5
mdshahed <mdshahedrahman14@gmail.com>
2023-03-03 16:22:19 +0600

    show more done

--- index.js
+++ index.js
@@ -2,20 +2,22 @@
 const loadData = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools')
         .then((res) => res.json())
-        .then((data) => showData(data.data))
+        .then((data) => showData(data.data.tools.slice(0, 6)))
     // .catch((err)=>{
     //     console.log(err)
     // })
+   
 }
-number = 0
+
 
 const showData = (data) => {
     // console.log(data.tools.slice(0,5))
     const cardContainer = document.getElementById('card-container')
+    cardContainer.innerHTML=''
     //   console.log(data.tools[0].image)
-    number += 1
-    data.tools.slice(0, 6).forEach(data => {
-        console.log(data)
+    
+    data.forEach(data => {
+        // console.log(data)
         const div = document.createElement('div')
         div.innerHTML = `
     <div class="card card-compact p-4 w-full h-full bg-base-100 shadow-2xl">
@@ -25,7 +27,7 @@ const showData = (data) => {
         <ol>
         <li>${'1'}${'.'} ${data.features[0]}</li>
         <li>${'2'}${'.'} ${data.features[1]}</li>
-        <li>${'3'}${'.'} ${data.features[2]}</li>
+         <li>${'3'}${'.'} ${data.features[2] ?data.features[2]:'feature not found'}</li>
         </ol>
         <hr>
       <div class="grid grid-cols-2 items-center space-x-24 ">
@@ -34,7 +36,7 @@ const showData = (data) => {
             <p><i class="fa-regular fa-calendar-days"></i> ${data.published_in}</p>
         </div>
         <div class="text-red-400">
-        <button><i class="fa-solid fa-arrow-right"></i></button>
+        <button onclick="showDetails('${data.description}')"><i class="fa-solid fa-arrow-right"></i></button>
         </div>
       </div>
     </div>
@@ -43,4 +45,17 @@ const showData = (data) => {
         cardContainer.appendChild(div)
     });
 }
+const showDetails =(id)=>{
+    const url=`https://openapi.programming-hero.com/api/ai/tool/${id}`
+    fetch(url)
+    .then((res) => res.json())
+    .then((data )=> console.log(data))
+    console.log(url)   
+}
 loadData()
+
+const showMoreData=()=>{
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+        .then((res) => res.json())
+        .then((data) => showData(data.data.tools))
+}
